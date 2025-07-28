714d11c3df91f8aef82589c1d5a6663d4a519314
marinicadenisandrei <marinicadenisandrei@gmail.com>
2025-07-28 14:54:05 +0300

    create func parse_log_file for processing the logs.log file

--- utils.py
+++ utils.py
@@ -3,3 +3,9 @@ from collections import defaultdict
 
 # Time format in the log
 TIME_FORMAT = "%H:%M:%S"
+
+def parse_log_file(log_path):
+    """
+    Parses the log file and returns a dictionary of job start and end events.
+    """
+    pass
